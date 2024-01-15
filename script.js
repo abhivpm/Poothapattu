2cb3a84e52c04da4e776115c77b62c28ccbe2cdb
Abhinav <80020642+abhivpm@users.noreply.github.com>
2024-01-15 12:48:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let currentBatch = 1;
 const batchSize = Math.ceil(frameCount / framesPerBatch);
 
 const currentFrame = index => (
-  `Sequence Webp/image_sequence_${index.toString().padStart(4, '0')}.jpg`
+  `Sequence_Webp_1/image_sequence_${index.toString().padStart(4, '0')}.jpg`
 );
 
 const preloadImages = (start, end) => {
